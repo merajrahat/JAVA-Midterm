f9e710eb319155c85264b8a000b54b8ffa3f5995
merajrahat <merajrahat@yahoo.com>
2020-11-08 18:00:25 -0500

    Factorial

--- src/math/problems/Factorial.java
+++ src/math/problems/Factorial.java
@@ -1,13 +1,40 @@
 package math.problems;
 
+
+
 public class Factorial {
 
+    static int factorial1(int n)
+    {
+        int res = 1, i;
+        for (i = 2; i <= n; i++)
+            res *= i;
+        return res;
+    }
     public static void main(String[] args) {
         /*
          * Factorial of 5! = 5 x 4 X 3 X 2 X 1 = 120.
          * Write a java program to find Factorial of a given number using Recursion as well as Iteration.
          *
          */
+        //Scanner object for capturing the user input
+        int num = 5;
+        System.out.println("Using Iteration Loop Factorial of " + num + " is " + factorial1(5));
+
+
+    int factorial = fact(5);
+        System.out.println("Using Recursion Factorial of 5 is: "+factorial);
+    }
+    static int fact(int n)
+    {
+        int output;
+        if(n==1){
+            return 1;
+        }
+        //Recursion: Function calling itself!!
+        output = fact(n-1)* n;
+        return output;
 
     }
+
 }
--- src/string/problems/DetermineLargestWord.java
+++ src/string/problems/DetermineLargestWord.java
@@ -10,9 +10,6 @@ public class DetermineLargestWord {
          Implement to Find the length and longest word in the given sentence below.
          Should return "10 biological".
          */
-
-
-
         String s = "Human brain is a biological learning machine";
         Map<Integer, String> wordNLength = findTheLargestWord(s);
         //implement
